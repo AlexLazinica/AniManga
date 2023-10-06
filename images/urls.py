5f9d2c794bcf3b0cc467dd456a37fbb51b99adca
AlexLazinica <alexlazinica2703@gmail.com>
2023-10-06 16:56:44 +0200

    update za mangu

--- images/urls.py
+++ images/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('anime-images/', views.anime_images, name='anime_images'),
+    path('manga-images/', views.manga_images, name='manga_images'),
 ]
--- images/views.py
+++ images/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from .api import fetch_anime_images
+from .api import fetch_manga_images
 
 def anime_images(request):
     images = fetch_anime_images()
     return render(request, 'image/anime_images.html', {'images': images})
+
+def manga_images(request):
+    images = fetch_manga_images()
+    return render(request, 'image/manga_images.html', {'images': images})
