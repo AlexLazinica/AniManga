d69b28c0bcc842c8bdad65c92dc629ba479250fb
AlexLazinica <alexlazinica2703@gmail.com>
2023-10-07 18:19:44 +0200

    views images change

--- images/views.py
+++ images/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from .api import fetch_anime_images
-from .api import fetch_manga_images
+from animanga_user.images.api import fetch_anime_data
+from animanga_user.images.api import fetch_manga_data
 
 def anime_images(request):
-    images = fetch_anime_images()
+    images = fetch_anime_data()
     return render(request, 'image/anime_images.html', {'images': images})
 
 def manga_images(request):
-    images = fetch_manga_images()
+    images = fetch_manga_data()
     return render(request, 'image/manga_images.html', {'images': images})
